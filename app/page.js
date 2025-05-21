c13e6c2611e969b4ebdd24c6ec34a55f847416bf
Anoj <anojrawal12345@gmail.com>
2025-05-22 00:44:47 +0530

    Front page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Dashbaord from "./dashboard/page";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const route = useRouter();
   return (
    <div>
-     
+     Hello and Welcome
+     <Button onClick={route}>DashBoard</Button>
    </div>
   );
 }
